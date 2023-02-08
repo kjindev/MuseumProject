7e66fe1d669e8b18dcfb5150ba9638a485ae70ce
kjin <pkjin.eng@gmail.com>
2023-02-08 18:20:34 +0900

    basename

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { QueryClient, QueryClientProvider } from "react-query";
 const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
         <App />
